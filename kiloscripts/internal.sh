74a33151ca011e23e773eece22a27fbc5dba5519
Nicholas Hart <nic@hdb3.net>
2016-01-26 17:13:06 +0000

    execute enable internal and external

--- kiloscripts/internal.sh
+++ kiloscripts/internal.sh
@@ -1,7 +1,8 @@
-ovs-vsctl del-port br-ex eth2
-ip addr add 172.16.1.1 dev br-ex
-ip link set dev br-ex up
-route add -net 172.16.1.0/24 gw 172.16.1.1
-crudini --set --verbose /etc/sysctl.conf "" net.ipv4.ip_forward 1
-sysctl -p
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#!/bin/bash -e
+sudo ovs-vsctl del-port br-ex eth2
+sudo ip addr add 172.16.1.1 dev br-ex
+sudo ip link set dev br-ex up
+sudo route add -net 172.16.1.0/24 gw 172.16.1.1
+sudo crudini --set --verbose /etc/sysctl.conf "" net.ipv4.ip_forward 1
+sudo sysctl -p
+sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
