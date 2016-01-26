7676915e6a6d264a1cdd899a9b551a814d2f5e0f
Nicholas Hart <nic@hdb3.net>
2016-01-26 17:10:28 +0000

    simple.sh delete the two VMs...

--- kiloscripts/simple.sh
+++ kiloscripts/simple.sh
@@ -5,4 +5,4 @@ export NETID=`neutron net-list| awk '/ testnet / {print $2}'`
 neutron subnet-create --name testnet testnet 172.16.42.0/24
 nova boot --flavor m1.small --image cirros-0.3.3-x86_64 --nic net-id=$NETID --poll testvm1
 nova boot --flavor m1.small --image cirros-0.3.3-x86_64 --nic net-id=$NETID --poll testvm2
-echo "nova delete testvm ; neutron subnet-delete testnet ; neutron net-delete testnet"
+echo "nova delete testvm1 testvm2 ; neutron subnet-delete testnet ; neutron net-delete testnet"
