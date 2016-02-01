5d2c1f4a3498904e21f96da4d2f03d25b27866b5
nic <nic@dell4>
2016-02-01 18:56:11 +0000

    liberty/remote.sh kiloscripts/remote.sh were not wanted\!

--- liberty/remote.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-for node in "$@"
-do
-    echo "$node"
-    ssh-keygen -f "/home/nic/.ssh/known_hosts" -R `dig +short $node`
-    ssh-keygen -f "/home/nic/.ssh/known_hosts" -R $node
-    scp *sh config services roles do_it subnet.py role.py patch.horizon $node:
-    ssh -t $node ./do_it
-done
