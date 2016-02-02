0b2b262967a2bd035dece1e1fcc4411be20276d6
Nicholas Hart <nic@hdb3.net>
2016-02-02 18:18:22 +0000

    use ip route

--- kiloscripts/internal.sh
+++ kiloscripts/internal.sh
@@ -2,7 +2,8 @@
 sudo ovs-vsctl del-port br-ex eth2
 sudo ip addr add 172.16.1.1 dev br-ex
 sudo ip link set dev br-ex up
-sudo route add -net 172.16.1.0/24 gw 172.16.1.1
+sudo ip route add 172.16.1.0/24 via 172.16.1.1
+# sudo route add -net 172.16.1.0/24 gw 172.16.1.1
 sudo crudini --set --verbose /etc/sysctl.conf "" net.ipv4.ip_forward 1
 sudo sysctl -p
 sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
