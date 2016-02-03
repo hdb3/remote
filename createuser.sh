aad7f57f9bbaef09dc436c336c9d8b8975751c11
nic <nic@dell4>
2016-02-03 14:50:55 +0000

    createuser.sh

--- createuser.sh
+++ createuser.sh
@@ -1,1 +1,1 @@
-useradd -d /home/centos -m centos && echo centos:centos | chpasswd
+useradd -d /home/centos -m centos && echo centos:centos | chpasswd && echo 'centos ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
