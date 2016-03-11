51b0ccb82659b2c61ca7af8bbaf92f79e2c3db52
Nicholas Hart <nic@hdb3.net>
2016-03-11 15:31:11 +0000

    update image scripts

--- openstack/ubuntu-image.sh
+++ openstack/ubuntu-image.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget https://cloud-images.ubuntu.com/wily/current/wily-server-cloudimg-amd64-disk1.img
-glance image-create --name "ubuntu-15.10" --file wily-server-cloudimg-amd64-disk1.img --disk-format qcow2 --container-format bare --progress
+curl https://cloud-images.ubuntu.com/wily/current/wily-server-cloudimg-amd64-disk1.img | glance image-create --name "ubuntu-15.10" --disk-format qcow2 --container-format bare --progress
