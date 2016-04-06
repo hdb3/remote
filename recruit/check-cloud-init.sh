afe19d4dad7208b05e17c87cbb7c26fa43ba7a68
nic <nic@dell4>
2016-04-06 16:56:02 +0100

    working recruit/check-cloud-init.sh with password login too - debug verbosity

--- recruit/check-cloud-init.sh
+++ recruit/check-cloud-init.sh
@@ -1,9 +1,58 @@
 #!/bin/bash
 
-defaultkey="/home/nic/.ssh/openstack_rsa"
-host="$1"
+    defaultkey="/home/nic/.ssh/openstack_rsa"
+    host="$1"
 
-SSH=$(which ssh)
+
+attempt_login() {
+
+# assumes that $host and $user is already set
+# if $key is set then try key authentication
+# otherwise  if $passwd is set try password authentication
+
+    if [[ -z "$user" ]]
+      then echo "user not set!"
+      exit
+    fi
+
+    if [[ -z "$host" ]]
+      then echo "user not set!"
+      exit
+    fi
+
+    SSHOPTIONS="-q -tt"
+    SSHOPTIONS="$SSHOPTIONS -o UserKnownHostsFile=/dev/null"
+    SSHOPTIONS="$SSHOPTIONS -o StrictHostKeyChecking=no"
+    SSHOPTIONS="$SSHOPTIONS -F /dev/null"
+
+    CMD="$(which ssh)"
+    if [[ -n "$key" ]]
+      then
+        XOPTS="-i${key} -o PasswordAuthentication=no"
+      elif [[ -n "$passwd" ]]
+      then
+        XOPTS="-o PasswordAuthentication=yes"
+        which sshpass || ( echo "please install sshpass" ; exit )
+        CMD="$(which sshpass) -p $passwd $CMD"
+      else
+        echo "attempt_login called with neither\$key nor \$passwd set"
+        return 1
+    fi
+
+    # only get here if we have a key or a password
+
+    XCMD="${CMD} ${SSHOPTIONS} ${XOPTS} -l ${user} ${host} sudo whoami"
+    ruser=$(${XCMD})
+
+    if [[ "$ruser" =~ "root" ]]
+      then
+        echo "attempt_login() succeeded with user ${user}"
+        return 0
+      else
+        echo "attempt_login() failed with user ${user}"
+        return 1
+    fi
+}
 
 which fping && fping $host
 
@@ -17,27 +66,34 @@ fi
 
 if [[ -f "${cloudkey}" ]]
   then
-    SSHOPTIONS="-o UserKnownHostsFile=/dev/null"
-    SSHOPTIONS="$SSHOPTIONS -o PasswordAuthentication=no"
-    SSHOPTIONS="$SSHOPTIONS -o StrictHostKeyChecking=no"
-    SSHOPTIONS="$SSHOPTIONS -F /dev/null"
-    SSHOPTIONS="$SSHOPTIONS -i${cloudkey}"
-    SSHOPTIONS="$SSHOPTIONS -q -tt"
+    key="${cloudkey}"
     for user in cirros centos ubuntu
     do
-      CMD="$SSH ${SSHOPTIONS} -l ${user} ${host} sudo whoami"
-      # echo "trying $CMD"
-      ruser=$(${CMD})
-      if [[ "$ruser" =~ "root" ]]
+      if attempt_login
         then
           echo "succeeded with user ${user}"
           xuser="${user}"
+          break
         else
           echo "failed with user ${user}"
       fi
     done
   else
     echo "the cloud key file (${cloudkey}) was not found"
+fi
+if [[ -z "$xuser" ]]
+  then
+    echo "trying password based login"
+    unset key
+    passwd="root"
+    user="root"
+    if attempt_login
+      then
+        echo "succeeded with user ${user}"
+        xuser="${user}"
+      else
+        echo "failed with user ${user}"
+    fi
 fi
 
 if [[ -z $xuser ]]
