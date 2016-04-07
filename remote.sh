d80cfe6819dc63044e543b089ddefa0c74405d8a
nic <nic@dell4>
2016-04-07 11:26:03 +0100

    cleanup, + copy a .ssh directory if it exists

--- remote.sh
+++ remote.sh
@@ -22,23 +22,14 @@ if [ -f $wd/user ]; then
   user="$user@"
 fi
 
-function sshclean {
-    echo "sshclean $1"
-    # ssh-keygen -q -f "~/.ssh/known_hosts" -R `dig +short $1`
-    # ssh-keygen -q -f "~/.ssh/known_hosts" -R $1
-}
-
 for node in "$@"
 do
     echo "$node (${user}${node})"
-
-    # if [ -f $wd/password ]; then
-      # which sshpass || ( echo "please install sshpass" ; exit )
-      # sshpass -p `cat $wd/password` ssh-copy-id ${user}${node}
-    # fi
-
-    # sshclean $node || echo "sshclean?"
-    echo "scp $wd/* ${user}${node}:"
     scp $wd/* ${user}${node}:
+    if [ -d $wd/.ssh ]; then
+        scp -r $wd/.ssh ${user}${node}:
+    fi
     ssh -t ${user}${node} sudo bash -ve do_it
 done
+
+
