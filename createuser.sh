e53997176c47e8b8fd2b43d1933029b6fd05a341
nic <nic@dell4>
2016-04-12 17:08:15 +0100

    encrypt the key

--- createuser.sh
+++ createuser.sh
@@ -1,1 +1,5 @@
-useradd -d /home/centos -m centos && echo centos:centos | chpasswd && echo 'centos ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+#!/bin/bash -e
+
+if [ "$#" -lt "1" ]; then echo "please give at least a remote host name"; exit ; fi
+if [ "$#" -lt "2" ]; then user="centos" ; else user="$2" ; fi
+echo "useradd -d /home/${user} -m ${user} && echo ${user}:${user} | chpasswd && echo '${user} ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers" | ssh -tt root@$1
--- recruit/client-install.sh
+++ recruit/client-install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 
-if [ -s recruit/adminkey ] ; then
+if [ -s recruit/adminkey.gpg ] ; then
+  which gpg || ( echo "please install gpg" ; exit )
   grep '^IdentityFile.*id_rsa' ~/.ssh/config >> /dev/null || \
     ( echo "adding id_rsa to .ssh/config" ; echo "IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config )
   grep '^IdentityFile.*adminkey' ~/.ssh/config >> /dev/null || \
     ( echo "adding adminkey to .ssh/config" ; echo "IdentityFile ~/.ssh/adminkey" >> ~/.ssh/config )
-  cp -vn recruit/adminkey ~/.ssh/
+  gpg -q recruit/adminkey.gpg
+  mv -vn recruit/adminkey ~/.ssh/ || rm recruit/adminkey
 else
   echo "recruit/adminkey is not accessible, so I can't install it for you"
 fi
