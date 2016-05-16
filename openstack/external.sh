082fd27f2943af9252d17bce29f6a34843b77d0e
nic <nic@dell4>
2016-05-16 18:18:17 +0100

    fixes for metadata agent on separet neutron node.

--- openstack/external.sh
+++ openstack/external.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -ev
+#
+# this script assumes that the script 'simple.sh' has already been run
+# 'simple.sh' creates tow VMs and a virtual network and router, r1
+#
+# if that router did not already exist then the commented out lines of code could be used to do so
+#
 source creds
 neutron net-create ext-net --router:external --provider:physical_network external --provider:network_type flat
 neutron subnet-create --name ext-net --dns-nameserver 10.30.65.200 --enable-dhcp --gateway 172.16.1.1 --allocation-pool start=172.16.1.128,end=172.16.1.254 ext-net 172.16.1.0/24
-neutron router-create ext-net
-neutron router-interface-add ext-net testnet
-neutron router-gateway-set ext-net ext-net
+# neutron router-create ext-net
+# neutron router-interface-add ext-net testnet
+# neutron router-gateway-set ext-net ext-net
+neutron router-gateway-set r1 ext-net
 
 for VM in testvm1 testvm2
   do echo $VM
