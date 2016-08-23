1944f18a63f3aacbc83fed62d2b89a3c5a9ffdd4
nic hart <nic@xps13>
2016-08-23 23:06:39 +0100

    first simple boot on mitaka-ovs

--- openstack/glance.sh
+++ openstack/glance.sh
@@ -9,6 +9,7 @@ openstack service create --name glance --description "OpenStack Image service" i
 openstack endpoint create --region RegionOne image public http://$CONTROLLER_IP:9292
 openstack endpoint create --region RegionOne image internal http://$CONTROLLER_IP:9292
 openstack endpoint create --region RegionOne image admin http://$CONTROLLER_IP:9292
+
 #install glance
 
 crudini --set --verbose /etc/glance/glance-api.conf database connection mysql+pymysql://glance:$DBPASSWD@$CONTROLLER_IP/glance
@@ -22,14 +23,12 @@ crudini --set --verbose /etc/glance/glance-api.conf keystone_authtoken project_n
 crudini --set --verbose /etc/glance/glance-api.conf keystone_authtoken username glance
 crudini --set --verbose /etc/glance/glance-api.conf keystone_authtoken password $SERVICE_PWD
 crudini --set --verbose /etc/glance/glance-api.conf paste_deploy flavor keystone
-crudini --set --verbose /etc/glance/glance-api.conf DEFAULT notification_driver noop
-crudini --set --verbose /etc/glance/glance-api.conf DEFAULT verbose True
 
 crudini --set --verbose /etc/glance/glance-api.conf glance_store stores file,http
 crudini --set --verbose /etc/glance/glance-api.conf glance_store default_store file
 crudini --set --verbose /etc/glance/glance-api.conf glance_store filesystem_store_datadir /var/lib/glance/images/
 
-crudini --set --verbose /etc/glance/glance-registry.conf database connection mysql+pymsql://glance:$DBPASSWD@$CONTROLLER_IP/glance
+crudini --set --verbose /etc/glance/glance-registry.conf database connection mysql+pymysql://glance:$DBPASSWD@$CONTROLLER_IP/glance
 crudini --set --verbose /etc/glance/glance-registry.conf keystone_authtoken auth_uri http://$CONTROLLER_IP:5000
 crudini --set --verbose /etc/glance/glance-registry.conf keystone_authtoken auth_url http://$CONTROLLER_IP:35357
 crudini --set --verbose /etc/glance/glance-registry.conf keystone_authtoken memcached_servers $CONTROLLER_IP:11211
@@ -39,9 +38,8 @@ crudini --set --verbose /etc/glance/glance-registry.conf keystone_authtoken user
 crudini --set --verbose /etc/glance/glance-registry.conf keystone_authtoken project_name service
 crudini --set --verbose /etc/glance/glance-registry.conf keystone_authtoken username glance
 crudini --set --verbose /etc/glance/glance-registry.conf keystone_authtoken password $SERVICE_PWD
+
 crudini --set --verbose /etc/glance/glance-registry.conf paste_deploy flavor keystone
-crudini --set --verbose /etc/glance/glance-registry.conf DEFAULT notification_driver noop
-crudini --set --verbose /etc/glance/glance-registry.conf DEFAULT verbose True
 
 #start glance
 su -s /bin/sh -c "glance-manage db_sync" glance
@@ -55,4 +53,10 @@ then
 else
   wget http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img
 fi
+# it can take a short while before glance service is available....
+until openstack image list
+do
+  echo "retrying image list..."
+done
+
 openstack image create --public --file cirros-0.3.4-x86_64-disk.img --disk-format qcow2 --container-format bare "cirros-0.3.3-x86_64"
