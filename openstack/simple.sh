83e00d38a817d847596b1c68db13dc074775ec55
Nicholas Hart <nic@hdb3.net>
2016-08-30 18:41:15 +0100

    finall mitaka is workking on distributed systems... problem was 1) security_group disabled waas BAD 2) server create command does NOT accept image and flavor names, only ids...!

--- openstack/simple.sh
+++ openstack/simple.sh
@@ -2,11 +2,19 @@
 source creds
 openstack keypair create --public-key openstack_rsa.pub openstack_rsa
 neutron net-create testnet
-export NETID=`neutron net-list| awk '/ testnet / {print $2}'`
 neutron subnet-create --name testnet testnet 172.16.42.0/24
 neutron router-create r1 # a router is needed in order to get metadata service over network
 neutron router-interface-add r1 testnet
-openstack server create --key-name openstack_rsa --flavor m1.tiny --image cirros-0.3.3-x86_64 --nic net-id=$NETID --wait testvm1
+for host in testvm1 testvm2
+  do
+  # the 'exit' in the awk avoids matching on more than one line...
+    openstack server create --wait \
+        --key-name openstack_rsa \
+        --flavor `openstack flavor list|awk '/tiny/ {print $2;exit}'` \
+        --image `openstack image list|awk '/cirros/ {print $2;exit}'` \
+        --nic net-id=`neutron net-list| awk '/testnet/ {print $2;exit}'` \
+        $host
+  done
 
 exit
 
