064364817e9394d87fee9d5dcc03d67491e50138
nic hart <nic@xps13>
2016-09-11 19:04:02 +0100

    build VM script really generic now...

--- openstack/build-vms.sh
+++ openstack/build-vms.sh
@@ -1,65 +1,69 @@
 #!/bin/bash
 BASEIMAGE="centos72-osbase"
 set -e
-COMPUTE_VMS="os-compute1 os-compute2"
-declare -A MAC
-MAC["os-compute1"]=52:54:00:27:a0:7f
-MAC["os-compute2"]=52:54:00:b0:c0:9a
-for vm in $COMPUTE_VMS ; do echo $vm ; echo "${MAC[$vm]}" ; done
+VMS="os-controller os-compute1 os-compute2"
+declare -A SPEC
+SPEC["os-compute1"]="52:54:00:27:a0:7f 4G 3 100G"
+SPEC["os-compute2"]="52:54:00:b0:c0:9a 4G 3 100G"
+SPEC["os-controller"]="52:54:00:6c:7b:24 2G 2 100G"
 
 function add_disk () {
   virsh vol-create-as default ${1}-${2} ${3} --format qcow2
   virsh attach-disk ${1} --source /var/lib/libvirt/images/${1}-${2} ${2} --driver qemu --subdriver qcow2 --config
 }
 
+function clone_ () {
+  if [[ $# -ne 5 ]] ; then
+    echo "problem with name or spec ($#) [$@]"
+    exit 1
+  fi
+  virt-clone -o $BASEIMAGE -n $1 --mac $2 --auto-clone
+  virsh setmaxmem $1 $3 --config
+  virsh setmem $1 $3 --config
+  virsh setvcpus $1 $4 --maximum --config
+  add_disk $1 vdb $5
+}
+
+function clone () {
+  clone_ $1 ${SPEC[$1]}
+}
+
+function destroy () {
+    virsh destroy ${1} || :
+    virsh undefine ${1} --nvram --remove-all-storage --snapshots-metadata || :
+}
+
+function ccheck () {
+  echo  -n "waiting for ping ($1)"
+  until fping $1 &> /dev/null
+    do
+      echo -n "."
+    done
+  echo "done ($1)"
+  echo  -n "waiting for ssh ($1)"
+  set +e
+  su  -p -c "ssh-keygen -f ~/.ssh/known_hosts -R $vm" $SUDO_USER > /dev/null
+  ssh-keygen -f /root/.ssh/known_hosts -R $vm > /dev/null
+  set -e
+  sshpass -p root ssh -q -o ConnectionAttempts=100 -o ConnectTimeout=1 root@${1} pwd > /dev/null
+  echo "done"
+}
+
 if [[ $EUID -ne 0 ]]; then
    echo "you probably want to run this script as root" 
-exit 1
+   exit 1
 fi
-for vm in os-controller $COMPUTE_VMS
-  do
-    virsh destroy $vm || :
-    virsh undefine $vm --nvram --remove-all-storage --snapshots-metadata || :
-  done
 
-virt-clone -o $BASEIMAGE -n os-controller --mac 52:54:00:6c:7b:24 --auto-clone
-#virsh vol-create-as default os-controller-vdb 256G --format qcow2
-#virsh attach-disk os-controller --source /var/lib/libvirt/images/os-controller-vdb vdb --driver qemu --subdriver qcow2 --config
-add_disk os-controller vdb 100G
-#virsh attach-interface --domain os-controller --mac 52:54:00:2d:d7:95 --type bridge --source br1 --config --persistent
-#virsh attach-interface --domain os-controller --mac 52:54:00:64:04:95 --type bridge --source br2 --config --persistent
-virsh setmaxmem os-controller 2G --config
-virsh setmem os-controller 2G --config
-
-for vm in $COMPUTE_VMS
+for vm in $VMS
   do
-    virt-clone -o $BASEIMAGE -n $vm --mac ${MAC[$vm]} --auto-clone
-    virsh setmaxmem $vm 4G --config
-    virsh setmem $vm 4G --config
-    virsh setvcpus $vm 3 --maximum --config
-    #virsh vol-create-as default $vm-vdb 256G --format qcow2
-    #virsh attach-disk $vm --source /var/lib/libvirt/images/$vm-vdb vdb --driver qemu --subdriver qcow2 --config
-    add_disk $vm vdb 100G
-  done
-
-for vm in os-controller $COMPUTE_VMS
-  do
+    destroy $vm
+    clone $vm
     virsh start $vm
-    set +e
-    su  -p -c "ssh-keygen -f ~/.ssh/known_hosts -R $vm" $SUDO_USER
-    ssh-keygen -f /root/.ssh/known_hosts -R $vm
-    set -e
   done
 
-echo  -n "waiting for ping"
-until fping os-controller os-compute1 &> /dev/null
+for vm in $VMS
   do
-    echo -n "."
+    ccheck $vm
   done
-echo "done"
-
-echo -n "checking ssh access...."
-sshpass -p root ssh -q -o ConnectionAttempts=100 -o ConnectTimeout=1 root@os-controller pwd > /dev/null
-echo "done"
 
 echo "all VMs built OK and running....."
