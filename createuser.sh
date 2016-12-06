593643d650b55db8cbb085223e5a4b08484ef3ab
nic hart <nic@hdb3.net>
2016-12-06 17:39:27 +0000

    createuser really works now

--- createuser.sh
+++ createuser.sh
@@ -1,9 +1,15 @@
-#!/bin/bash -exv
+#!/bin/bash -ex
 
-if [ "$#" -lt "1" ]; then echo "please give at least a remote host name; optionally also the new username and the remote sudo user to work as"; exit ; fi
-if [ "$#" -lt "2" ]; then user="centos" ; else user="$2" ; fi
-if [ "$#" -lt "3" ]; then remote_user="root" ; else remote_user="$3" ; fi
-echo "sudo useradd -d /home/${user} -m ${user}" | ssh -t $remote_user@$1
-echo "echo ${user}:${user} | sudo chpasswd " | ssh -t $remote_user@$1
-echo "echo \"${user} ALL=(ALL) NOPASSWD: ALL\" sudo tee -a /etc/sudoers" | ssh -t $remote_user@$1
-#echo "useradd -d /home/${user} -m ${user} && echo ${user}:${user} | chpasswd && echo '${user} ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers" | ssh -tt root@$1
+# this utility requires a root capable user rather than a sudo user
+# because in many cases, e.g. RHEL/Centos, sudo is not available via remote ssh sessions until the suoders configuration is updated
+
+if [ "$#" -lt "1" ]; then echo "usage: $0 hostname root-password new-user remote-root-user" ; exit ; fi
+if [ "$#" -lt "2" ]; then remote_root_passwd="root" ; else remote_root_passwd="$2" ; fi
+if [ "$#" -lt "3" ]; then user="centos" ; else user="$3" ; fi
+if [ "$#" -lt "4" ]; then remote_root_user="root" ; else remote_root_user="$4" ; fi
+echo "setting up user:$user on host:$1 using remote root credentials $remote_root_user:$remote_root_passwd"
+
+password_hash=`openssl passwd $user`
+sshpass -p $remote_root_passwd ssh ${remote_root_user}@${1} useradd -p ${password_hash} -m $user
+sshpass -p $remote_root_passwd ssh ${remote_root_user}@${1} "echo '$user ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
+sshpass -p $remote_root_passwd ssh ${remote_root_user}@${1} "sed -i -e '/requiretty/ s/^Defaults/#Defaults/' /etc/sudoers"
