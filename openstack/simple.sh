5948f8a530d7ec036a9d36d3b6a5e0932abc7a13
Nicholas Hart <nic@hdb3.net>
2017-02-10 09:48:46 +0000

    simple does not build a network anymore

--- openstack/simple.sh
+++ openstack/simple.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -ev
+# this script assumes that an image 'cirros' exists
+# and that a network 'default' exists
 source creds
 openstack keypair create --public-key openstack_rsa.pub openstack_rsa
-neutron net-create testnet
-neutron subnet-create --name testnet testnet 172.16.42.0/24
-neutron router-create r1 # a router is needed in order to get metadata service over network
-neutron router-interface-add r1 testnet
 for host in testvm1 testvm2
   do
   # the 'exit' in the awk avoids matching on more than one line...
@@ -12,14 +10,13 @@ for host in testvm1 testvm2
         --key-name openstack_rsa \
         --flavor `openstack flavor list|awk '/tiny/ {print $2;exit}'` \
         --image `openstack image list|awk '/cirros/ {print $2;exit}'` \
-        --nic net-id=`neutron net-list| awk '/testnet/ {print $2;exit}'` \
+        --nic net-id=`neutron net-list| awk '/default/ {print $2;exit}'` \
         $host
   done
-
 exit
 
 openstack keypair delete openstack_rsa
-openstack server delete testvm1
+openstack server delete testvm1 testvm2
 neutron router-interface-delete r1 testnet
 neutron router-delete r1
 neutron subnet-delete testnet
