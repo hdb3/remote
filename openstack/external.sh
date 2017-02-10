821aff488eb499045d62d4967067c4e3d1f303c3
nic hart <nic@hdb3.net>
2017-02-10 09:45:14 +0000

    add external address pool support

--- openstack/external.sh
+++ openstack/external.sh
@@ -2,12 +2,43 @@
 #
 source creds
 source config
-#INTNET=10.30.64.0/24 INTNAME=net64 INTGW=10.30.65.12
-#DNS=10.30.65.200 GW=10.30.65.1 NET=10.30.65.0/24
-neutron net-create $EXTERNAL_PORT --router:external --provider:physical_network $EXTERNAL_PORT --provider:network_type flat
-neutron subnet-create --name subnet-${EXTERNAL_PORT} --dns-nameserver $DNS --disable-dhcp --gateway $GW $EXTERNAL_PORT $NET
-neutron net-create $INTNAME
-neutron subnet-create --name $INTNAME $INTNAME $INTNET
-neutron router-create router-${EXTERNAL_PORT}
-neutron router-interface-add router-${EXTERNAL_PORT} ${INTNAME}
-neutron router-gateway-set --disable-snat --fixed-ip ip_address=$INTGW router-${EXTERNAL_PORT} $EXTERNAL_PORT
+
+# build an external network if the config gives us the relavant configuration
+if [[ -n "$DNS" && -n "$NET" && -n "$GW" && -n "$EXTERNAL_PORT" ]] ; then
+   echo "Building an external network on $EXTERNAL_PORT"
+   neutron net-create $EXTERNAL_PORT --router:external --provider:physical_network $EXTERNAL_PORT --provider:network_type flat
+   if [[ -n "$POOLSTART" && -n "$POOLEND" ]] ; then
+       neutron subnet-create --name subnet-${EXTERNAL_PORT} --allocation-pool start=$POOLSTART,end=$POOLEND --dns-nameserver $DNS --disable-dhcp --gateway $GW $EXTERNAL_PORT $NET
+   else
+       neutron subnet-create --name subnet-${EXTERNAL_PORT} --dns-nameserver $DNS --disable-dhcp --gateway $GW $EXTERNAL_PORT $NET
+   fi
+
+   # only build the internal networks if there is a valid external network to hook into
+
+   # a externally routed internal network which does not need floating IPs
+   if [[ -n "$INTNAME" && -n "$INTNET" && -n "$EXTERNAL_PORT" ]] ; then
+      echo "Building a routed internal network"
+   neutron net-create $INTNAME
+   neutron subnet-create --name $INTNAME $INTNAME $INTNET
+   neutron router-create router-${INTNAME}
+   neutron router-interface-add router-${INTNAME} ${INTNAME}
+   neutron router-gateway-set --disable-snat --fixed-ip ip_address=$INTGW router-${INTNAME} $EXTERNAL_PORT
+   else
+      echo "Not building a routed internal network"
+   fi
+
+   # an internal network which needs floating IPs
+   if [[ -n "$FLOATNAME" && -n "$FLOATNET" && -n "$EXTERNAL_PORT" ]] ; then
+      echo "Building a default internal network"
+       neutron net-create $FLOATNAME
+       neutron subnet-create --name $FLOATNAME $FLOATNAME $FLOATNET
+       neutron router-create router-${FLOATNAME}
+       neutron router-interface-add router-${FLOATNAME} ${FLOATNAME}
+       neutron router-gateway-set router-${FLOATNAME} $EXTERNAL_PORT
+   else
+      echo "Not building a default internal network"
+   fi
+
+else
+   echo "Not building an external network"
+fi
